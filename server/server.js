9ad6e0f8bb9caeb72dc8f1168a6072dc9f66d8a2
Krzysztof Pielak <krzysztofp@advaoptical.com>
2018-07-24 13:52:29 +0200

    first socket events

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 require('./config/config');
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
 const publicPath = path.join(__dirname, '../public');
 
@@ -8,10 +10,23 @@ console.log(`${__dirname}/../public`);
 console.log(publicPath);
 
 const app = express();
+const server = http.createServer(app);
+//creating socketIO instance, passing server as an argument to inidcate, that we want to run sockets on this server instance
+//io - web socket server
+const io = socketIO(server);
+
 //used middleware to serve frontend public html
 app.use(express.static(publicPath));
 
+//registering an io event
+io.on('connection', socket => {
+    console.log('New user connected');
+    socket.on('disconnecting', socket => {
+        console.log('User disconnected');
+    });
+});
+
 //listen to the requests
-app.listen(process.env.PORT, () => {
+server.listen(process.env.PORT, () => {
     console.info(`Server is on port ${process.env.PORT}`);
 });
